b73079c27a0062d1762f2239abda1811457807e3
Hussein <55514845+hu-kh@users.noreply.github.com>
2019-10-04 23:59:33 +0200

    preloader created

---
+++ README.md
@@ -0,0 +1,2 @@
+## Preloader
+Simple Preloader build with css and Pure js
---
+++ preloader/vendor/js/app.js
@@ -0,0 +1,5 @@
+/**
+ * Preloader
+ */
+let t;function a() { t = setTimeout(b, 1000);}a();function b() {document.getElementById("loader").style.display = "none";document.getElementById("content").style.display = "block";}
+
